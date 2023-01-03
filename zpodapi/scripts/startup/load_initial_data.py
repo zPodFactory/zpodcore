94d1022da80be7487f6ba5eaec189c205f860f1c
Timo Sugliani <timo.sugliani@gmail.com>
2023-01-03 16:50:28 +0100

    Set simple token for dev purposes, and set last_connection to avoid current potential issues with zpodsdk
    Signed-off-by: Timo Sugliani <timo.sugliani@gmail.com>

--- zpodapi/scripts/startup/load_initial_data.py
+++ zpodapi/scripts/startup/load_initial_data.py
@@ -5,17 +5,22 @@ from datetime import datetime
 
 from zpodapi.lib import database
 from zpodapi.models import User
+from zpodapi.settings import settings
 
 session = database.get_session_one()
 userCnt = session.query(User).count()
 if not userCnt:
+    # Create a super user with very simple api token for quick tests
+
     user = User(
         username="superuser",
         email="superuser@zpodfactory.io",
-        api_token=secrets.token_urlsafe(256),
+        api_token=("supertoken" if settings.DEV_MODE else secrets.token_urlsafe(32)),
         creation_date=datetime.now(),
+        last_connection=datetime.now(),
         superadmin=True,
     )
+
     print(
         f"Initial user created:\n"
         f"  Username: {user.username}\n"
