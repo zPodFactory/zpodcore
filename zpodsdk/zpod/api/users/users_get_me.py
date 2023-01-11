762af0e54d629949daf74d44c7be6e2572ea02ca
Kelby Valenti <kvalenti@vmware.com>
2023-01-11 10:46:45 -0500

    Make code completion work properly for sdk

--- zpodsdk/zpod/api/users/users_get_me.py
+++ zpodsdk/zpod/api/users/users_get_me.py
@@ -4,134 +4,119 @@ from typing import Any, Dict, Optional
 import httpx
 
 from ... import errors
-from ...client import AuthenticatedClient, Client
+from ...client import Client
 from ...models.user_view import UserView
 from ...types import Response
 
 
-def _get_kwargs(
-    *,
-    client: AuthenticatedClient,
-) -> Dict[str, Any]:
-    url = "{}/user/me".format(client.base_url)
+class UsersGetMe:
+    def __init__(self, client: Client) -> None:
+        self.client = client
 
-    headers: Dict[str, str] = client.get_headers()
-    cookies: Dict[str, Any] = client.get_cookies()
+    def _get_kwargs(
+        self,
+    ) -> Dict[str, Any]:
+        url = "{}/user/me".format(self.client.base_url)
 
-    return {
-        "method": "get",
-        "url": url,
-        "headers": headers,
-        "cookies": cookies,
-        "timeout": client.get_timeout(),
-    }
+        headers: Dict[str, str] = self.client.get_headers()
+        cookies: Dict[str, Any] = self.client.get_cookies()
 
+        return {
+            "method": "get",
+            "url": url,
+            "headers": headers,
+            "cookies": cookies,
+            "timeout": self.client.get_timeout(),
+        }
 
-def _parse_response(*, client: Client, response: httpx.Response) -> Optional[UserView]:
-    if response.status_code == HTTPStatus.OK:
-        response_200 = UserView.from_dict(response.json())
+    def _parse_response(self, *, response: httpx.Response) -> Optional[UserView]:
+        if response.status_code == HTTPStatus.OK:
+            response_200 = UserView.from_dict(response.json())
 
-        return response_200
-    if client.raise_on_unexpected_status:
-        raise errors.UnexpectedStatus(f"Unexpected status code: {response.status_code}")
-    else:
-        return None
+            return response_200
+        if self.client.raise_on_unexpected_status:
+            raise errors.UnexpectedStatus(
+                f"Unexpected status code:     {response.status_code}"
+            )
+        else:
+            return None
 
-
-def _build_response(*, client: Client, response: httpx.Response) -> Response[UserView]:
-    return Response(
-        status_code=HTTPStatus(response.status_code),
-        content=response.content,
-        headers=response.headers,
-        parsed=_parse_response(client=client, response=response),
-    )
-
-
-def sync_detailed(
-    *,
-    client: AuthenticatedClient,
-) -> Response[UserView]:
-    """Get Me
-
-    Raises:
-        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
-        httpx.TimeoutException: If the request takes longer than Client.timeout.
-
-    Returns:
-        Response[UserView]
-    """
-
-    kwargs = _get_kwargs(
-        client=client,
-    )
-
-    response = httpx.request(
-        verify=client.verify_ssl,
-        **kwargs,
-    )
-
-    return _build_response(client=client, response=response)
-
-
-def sync(
-    *,
-    client: AuthenticatedClient,
-) -> Optional[UserView]:
-    """Get Me
-
-    Raises:
-        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
-        httpx.TimeoutException: If the request takes longer than Client.timeout.
-
-    Returns:
-        Response[UserView]
-    """
-
-    return sync_detailed(
-        client=client,
-    ).parsed
-
-
-async def asyncio_detailed(
-    *,
-    client: AuthenticatedClient,
-) -> Response[UserView]:
-    """Get Me
-
-    Raises:
-        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
-        httpx.TimeoutException: If the request takes longer than Client.timeout.
-
-    Returns:
-        Response[UserView]
-    """
-
-    kwargs = _get_kwargs(
-        client=client,
-    )
-
-    async with httpx.AsyncClient(verify=client.verify_ssl) as _client:
-        response = await _client.request(**kwargs)
-
-    return _build_response(client=client, response=response)
-
-
-async def asyncio(
-    *,
-    client: AuthenticatedClient,
-) -> Optional[UserView]:
-    """Get Me
-
-    Raises:
-        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
-        httpx.TimeoutException: If the request takes longer than Client.timeout.
-
-    Returns:
-        Response[UserView]
-    """
-
-    return (
-        await asyncio_detailed(
-            client=client,
+    def _build_response(self, *, response: httpx.Response) -> Response[UserView]:
+        return Response(
+            status_code=HTTPStatus(response.status_code),
+            content=response.content,
+            headers=response.headers,
+            parsed=self._parse_response(response=response),
         )
-    ).parsed
+
+    def sync_detailed(
+        self,
+    ) -> Response[UserView]:
+        """Get Me
+
+        Raises:
+            errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+            httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+        Returns:
+            Response[UserView]
+        """
+
+        kwargs = self._get_kwargs()
+
+        response = httpx.request(
+            verify=self.client.verify_ssl,
+            **kwargs,
+        )
+
+        return self._build_response(response=response)
+
+    def sync(
+        self,
+    ) -> Optional[UserView]:
+        """Get Me
+
+        Raises:
+            errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+            httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+        Returns:
+            Response[UserView]
+        """
+
+        return self.sync_detailed().parsed
+
+    async def asyncio_detailed(
+        self,
+    ) -> Response[UserView]:
+        """Get Me
+
+        Raises:
+            errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+            httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+        Returns:
+            Response[UserView]
+        """
+
+        kwargs = self._get_kwargs()
+
+        async with httpx.AsyncClient(verify=self.client.verify_ssl) as _client:
+            response = await _client.request(**kwargs)
+
+        return self._build_response(response=response)
+
+    async def asyncio(
+        self,
+    ) -> Optional[UserView]:
+        """Get Me
+
+        Raises:
+            errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+            httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+        Returns:
+            Response[UserView]
+        """
+
+        return (await self.asyncio_detailed()).parsed
--- zpodsdk/zpod/api/users/users_update.py
+++ zpodsdk/zpod/api/users/users_update.py
@@ -4,202 +4,198 @@ from typing import Any, Dict, Optional, Union
 import httpx
 
 from ... import errors
-from ...client import AuthenticatedClient, Client
+from ...client import Client
 from ...models.http_validation_error import HTTPValidationError
 from ...models.user_update import UserUpdate
 from ...models.user_view import UserView
 from ...types import UNSET, Response, Unset
 
 
-def _get_kwargs(
-    *,
-    client: AuthenticatedClient,
-    json_body: UserUpdate,
-    username: Union[Unset, None, str] = UNSET,
-    email: Union[Unset, None, str] = UNSET,
-) -> Dict[str, Any]:
-    url = "{}/user".format(client.base_url)
+class UsersUpdate:
+    def __init__(self, client: Client) -> None:
+        self.client = client
 
-    headers: Dict[str, str] = client.get_headers()
-    cookies: Dict[str, Any] = client.get_cookies()
+    def _get_kwargs(
+        self,
+        *,
+        json_body: UserUpdate,
+        username: Union[Unset, None, str] = UNSET,
+        email: Union[Unset, None, str] = UNSET,
+    ) -> Dict[str, Any]:
+        url = "{}/user".format(self.client.base_url)
 
-    params: Dict[str, Any] = {}
-    params["username"] = username
+        headers: Dict[str, str] = self.client.get_headers()
+        cookies: Dict[str, Any] = self.client.get_cookies()
 
-    params["email"] = email
+        params: Dict[str, Any] = {}
+        params["username"] = username
 
-    params = {k: v for k, v in params.items() if v is not UNSET and v is not None}
+        params["email"] = email
 
-    json_json_body = json_body.to_dict()
+        params = {k: v for k, v in params.items() if v is not UNSET and v is not None}
 
-    return {
-        "method": "patch",
-        "url": url,
-        "headers": headers,
-        "cookies": cookies,
-        "timeout": client.get_timeout(),
-        "json": json_json_body,
-        "params": params,
-    }
+        json_json_body = json_body.to_dict()
 
+        return {
+            "method": "patch",
+            "url": url,
+            "headers": headers,
+            "cookies": cookies,
+            "timeout": self.client.get_timeout(),
+            "json": json_json_body,
+            "params": params,
+        }
 
-def _parse_response(
-    *, client: Client, response: httpx.Response
-) -> Optional[Union[HTTPValidationError, UserView]]:
-    if response.status_code == HTTPStatus.CREATED:
-        response_201 = UserView.from_dict(response.json())
+    def _parse_response(
+        self, *, response: httpx.Response
+    ) -> Optional[Union[HTTPValidationError, UserView]]:
+        if response.status_code == HTTPStatus.CREATED:
+            response_201 = UserView.from_dict(response.json())
 
-        return response_201
-    if response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY:
-        response_422 = HTTPValidationError.from_dict(response.json())
+            return response_201
+        if response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY:
+            response_422 = HTTPValidationError.from_dict(response.json())
 
-        return response_422
-    if client.raise_on_unexpected_status:
-        raise errors.UnexpectedStatus(f"Unexpected status code: {response.status_code}")
-    else:
-        return None
+            return response_422
+        if self.client.raise_on_unexpected_status:
+            raise errors.UnexpectedStatus(
+                f"Unexpected status code:     {response.status_code}"
+            )
+        else:
+            return None
 
+    def _build_response(
+        self, *, response: httpx.Response
+    ) -> Response[Union[HTTPValidationError, UserView]]:
+        return Response(
+            status_code=HTTPStatus(response.status_code),
+            content=response.content,
+            headers=response.headers,
+            parsed=self._parse_response(response=response),
+        )
 
-def _build_response(
-    *, client: Client, response: httpx.Response
-) -> Response[Union[HTTPValidationError, UserView]]:
-    return Response(
-        status_code=HTTPStatus(response.status_code),
-        content=response.content,
-        headers=response.headers,
-        parsed=_parse_response(client=client, response=response),
-    )
+    def sync_detailed(
+        self,
+        *,
+        json_body: UserUpdate,
+        username: Union[Unset, None, str] = UNSET,
+        email: Union[Unset, None, str] = UNSET,
+    ) -> Response[Union[HTTPValidationError, UserView]]:
+        """Update
 
+        Args:
+            username (Union[Unset, None, str]):
+            email (Union[Unset, None, str]):
+            json_body (UserUpdate):
 
-def sync_detailed(
-    *,
-    client: AuthenticatedClient,
-    json_body: UserUpdate,
-    username: Union[Unset, None, str] = UNSET,
-    email: Union[Unset, None, str] = UNSET,
-) -> Response[Union[HTTPValidationError, UserView]]:
-    """Update
+        Raises:
+            errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+            httpx.TimeoutException: If the request takes longer than Client.timeout.
 
-    Args:
-        username (Union[Unset, None, str]):
-        email (Union[Unset, None, str]):
-        json_body (UserUpdate):
+        Returns:
+            Response[Union[HTTPValidationError, UserView]]
+        """
 
-    Raises:
-        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
-        httpx.TimeoutException: If the request takes longer than Client.timeout.
-
-    Returns:
-        Response[Union[HTTPValidationError, UserView]]
-    """
-
-    kwargs = _get_kwargs(
-        client=client,
-        json_body=json_body,
-        username=username,
-        email=email,
-    )
-
-    response = httpx.request(
-        verify=client.verify_ssl,
-        **kwargs,
-    )
-
-    return _build_response(client=client, response=response)
-
-
-def sync(
-    *,
-    client: AuthenticatedClient,
-    json_body: UserUpdate,
-    username: Union[Unset, None, str] = UNSET,
-    email: Union[Unset, None, str] = UNSET,
-) -> Optional[Union[HTTPValidationError, UserView]]:
-    """Update
-
-    Args:
-        username (Union[Unset, None, str]):
-        email (Union[Unset, None, str]):
-        json_body (UserUpdate):
-
-    Raises:
-        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
-        httpx.TimeoutException: If the request takes longer than Client.timeout.
-
-    Returns:
-        Response[Union[HTTPValidationError, UserView]]
-    """
-
-    return sync_detailed(
-        client=client,
-        json_body=json_body,
-        username=username,
-        email=email,
-    ).parsed
-
-
-async def asyncio_detailed(
-    *,
-    client: AuthenticatedClient,
-    json_body: UserUpdate,
-    username: Union[Unset, None, str] = UNSET,
-    email: Union[Unset, None, str] = UNSET,
-) -> Response[Union[HTTPValidationError, UserView]]:
-    """Update
-
-    Args:
-        username (Union[Unset, None, str]):
-        email (Union[Unset, None, str]):
-        json_body (UserUpdate):
-
-    Raises:
-        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
-        httpx.TimeoutException: If the request takes longer than Client.timeout.
-
-    Returns:
-        Response[Union[HTTPValidationError, UserView]]
-    """
-
-    kwargs = _get_kwargs(
-        client=client,
-        json_body=json_body,
-        username=username,
-        email=email,
-    )
-
-    async with httpx.AsyncClient(verify=client.verify_ssl) as _client:
-        response = await _client.request(**kwargs)
-
-    return _build_response(client=client, response=response)
-
-
-async def asyncio(
-    *,
-    client: AuthenticatedClient,
-    json_body: UserUpdate,
-    username: Union[Unset, None, str] = UNSET,
-    email: Union[Unset, None, str] = UNSET,
-) -> Optional[Union[HTTPValidationError, UserView]]:
-    """Update
-
-    Args:
-        username (Union[Unset, None, str]):
-        email (Union[Unset, None, str]):
-        json_body (UserUpdate):
-
-    Raises:
-        errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
-        httpx.TimeoutException: If the request takes longer than Client.timeout.
-
-    Returns:
-        Response[Union[HTTPValidationError, UserView]]
-    """
-
-    return (
-        await asyncio_detailed(
-            client=client,
+        kwargs = self._get_kwargs(
             json_body=json_body,
             username=username,
             email=email,
         )
-    ).parsed
+
+        response = httpx.request(
+            verify=self.client.verify_ssl,
+            **kwargs,
+        )
+
+        return self._build_response(response=response)
+
+    def sync(
+        self,
+        *,
+        json_body: UserUpdate,
+        username: Union[Unset, None, str] = UNSET,
+        email: Union[Unset, None, str] = UNSET,
+    ) -> Optional[Union[HTTPValidationError, UserView]]:
+        """Update
+
+        Args:
+            username (Union[Unset, None, str]):
+            email (Union[Unset, None, str]):
+            json_body (UserUpdate):
+
+        Raises:
+            errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+            httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+        Returns:
+            Response[Union[HTTPValidationError, UserView]]
+        """
+
+        return self.sync_detailed(
+            json_body=json_body,
+            username=username,
+            email=email,
+        ).parsed
+
+    async def asyncio_detailed(
+        self,
+        *,
+        json_body: UserUpdate,
+        username: Union[Unset, None, str] = UNSET,
+        email: Union[Unset, None, str] = UNSET,
+    ) -> Response[Union[HTTPValidationError, UserView]]:
+        """Update
+
+        Args:
+            username (Union[Unset, None, str]):
+            email (Union[Unset, None, str]):
+            json_body (UserUpdate):
+
+        Raises:
+            errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+            httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+        Returns:
+            Response[Union[HTTPValidationError, UserView]]
+        """
+
+        kwargs = self._get_kwargs(
+            json_body=json_body,
+            username=username,
+            email=email,
+        )
+
+        async with httpx.AsyncClient(verify=self.client.verify_ssl) as _client:
+            response = await _client.request(**kwargs)
+
+        return self._build_response(response=response)
+
+    async def asyncio(
+        self,
+        *,
+        json_body: UserUpdate,
+        username: Union[Unset, None, str] = UNSET,
+        email: Union[Unset, None, str] = UNSET,
+    ) -> Optional[Union[HTTPValidationError, UserView]]:
+        """Update
+
+        Args:
+            username (Union[Unset, None, str]):
+            email (Union[Unset, None, str]):
+            json_body (UserUpdate):
+
+        Raises:
+            errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
+            httpx.TimeoutException: If the request takes longer than Client.timeout.
+
+        Returns:
+            Response[Union[HTTPValidationError, UserView]]
+        """
+
+        return (
+            await self.asyncio_detailed(
+                json_body=json_body,
+                username=username,
+                email=email,
+            )
+        ).parsed
