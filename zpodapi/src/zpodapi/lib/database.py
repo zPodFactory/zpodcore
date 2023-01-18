90721aed296198d26c45f305dc555f29d5f8c86b
Kelby Valenti <kvalenti@vmware.com>
2023-01-18 15:33:05 -0500

    Database updates

--- zpodapi/src/zpodapi/lib/database.py
+++ zpodapi/src/zpodapi/lib/database.py
@@ -1,19 +1,28 @@
+import logging
+from collections.abc import Generator
+from contextlib import contextmanager
+
 from sqlmodel import Session, SQLModel, create_engine
 
 from zpodapi import models  # noqa: F401
 from zpodapi import settings
 
-engine = create_engine(settings.POSTGRES_DSN)
+logger = logging.getLogger(__name__)
+
+engine = create_engine(
+    settings.POSTGRES_DSN,
+    echo=settings.ECHO_SQL,
+    echo_pool=settings.ECHO_POOL,
+)
 
 
 def create_tables():
     SQLModel.metadata.create_all(engine)
 
 
-def get_session():
+def get_session() -> Generator[Session, None]:
     with Session(engine) as session:
         yield session
 
 
-def get_session_one():
-    return next(get_session())
+get_session_ctx = contextmanager(get_session)
