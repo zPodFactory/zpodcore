5a008fed8036f527e89ca781adfe2abaaca1ca58
Kelby Valenti <kvalenti@vmware.com>
2023-01-18 15:33:05 -0500

    Database updates

--- zpodapi/scripts/startup/load_initial_data.py
+++ zpodapi/scripts/startup/load_initial_data.py
@@ -7,25 +7,27 @@ from zpodapi.lib import database
 from zpodapi.models import User
 from zpodapi.settings import settings
 
-session = database.get_session_one()
-userCnt = session.query(User).count()
-if not userCnt:
-    # Create a super user with very simple api token for quick tests
+with database.get_session_ctx() as session:
+    userCnt = session.query(User).count()
+    if not userCnt:
+        # Create a super user with very simple api token for quick tests
 
-    user = User(
-        username="superuser",
-        email="superuser@zpodfactory.io",
-        api_token=("supertoken" if settings.DEV_MODE else secrets.token_urlsafe(32)),
-        creation_date=datetime.now(),
-        last_connection=datetime.now(),
-        superadmin=True,
-    )
+        user = User(
+            username="superuser",
+            email="superuser@zpodfactory.io",
+            api_token=(
+                "supertoken" if settings.DEV_MODE else secrets.token_urlsafe(32)
+            ),
+            creation_date=datetime.now(),
+            last_connection=datetime.now(),
+            superadmin=True,
+        )
 
-    print(
-        f"Initial user created:\n"
-        f"  Username: {user.username}\n"
-        f"  Email: {user.email}\n"
-        f"  API Token: {user.api_token}\n"
-    )
-    session.add(user)
-    session.commit()
+        print(
+            f"Initial user created:\n"
+            f"  Username: {user.username}\n"
+            f"  Email: {user.email}\n"
+            f"  API Token: {user.api_token}\n"
+        )
+        session.add(user)
+        session.commit()
