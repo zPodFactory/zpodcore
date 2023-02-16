c71e2cf2ed85494cd3f52005f29634197fbcb534
Kelby Valenti <kvalenti@vmware.com>
2023-02-16 16:38:13 -0500

    Install db related packages

---
+++ zpodengine/src/zpodengine/samples/db_sample.py
@@ -0,0 +1,20 @@
+from prefect import flow, task
+from sqlmodel import select
+
+from zpodcommon import models as M
+from zpodengine.lib import database
+
+
+@task
+def get_users():
+    with database.get_session_ctx() as session:
+        print(session.exec(select(M.User)).all())
+
+
+@flow(log_prints=True)
+def db_sample():
+    get_users()
+
+
+if __name__ == "__main__":
+    print(db_sample())
