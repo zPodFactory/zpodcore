e12ce47c7a6ed772eb82caaec8caa208cba5c5ad
Kelby Valenti <kvalenti@vmware.com>
2023-02-16 14:58:07 -0500

    Created settings for zpodengine

---
+++ zpodengine/src/zpodengine/lib/database.py
@@ -0,0 +1,24 @@
+import logging
+from collections.abc import Generator
+from contextlib import contextmanager
+
+from sqlmodel import Session, create_engine
+
+from zpodcommon import models  # noqa: F401
+from zpodengine import settings
+
+logger = logging.getLogger(__name__)
+
+engine = create_engine(
+    settings.POSTGRES_DSN,
+    echo=settings.ECHO_SQL,
+    echo_pool=settings.ECHO_POOL,
+)
+
+
+def get_session() -> Generator[Session, None]:
+    with Session(engine) as session:
+        yield session
+
+
+get_session_ctx = contextmanager(get_session)
