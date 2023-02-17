731d91464831e853815fe73931e6fbc3cca4169a
Kelby Valenti <kvalenti@vmware.com>
2023-02-17 14:34:16 -0500

    Add COMPOSE_PROJECT_NAME to zpodengine docker image name.

--- zpodengine/scripts/create_blocks.py
+++ zpodengine/scripts/create_blocks.py
@@ -6,14 +6,16 @@ from prefect.infrastructure import DockerContainer
 
 ZPODCORE_PATH = os.environ["ZPODCORE_PATH"]
 ZPODENGINE_HOSTPORT = os.environ.get("ZPODENGINE_HOSTPORT", "localhost:4200")
+COMPOSE_PROJECT_NAME = os.environ.get("COMPOSE_PROJECT_NAME", "zpodcore")
+
 
 docker_block = DockerContainer(
-    image="zpodengine:v1",
+    image=f"{COMPOSE_PROJECT_NAME}-zpodengine:v1",
     auto_remove=True,
     volumes=[
         f"{ZPODCORE_PATH}/zpodcommon/src/zpodcommon:/zpodcore/src/zpodcommon",
         f"{ZPODCORE_PATH}/zpodengine/src/zpodengine:/zpodcore/src/zpodengine",
-        f"{ZPODCORE_PATH}/.env:/zpodcore/.env"
+        f"{ZPODCORE_PATH}/.env:/zpodcore/.env",
     ],
     env={
         "PREFECT_API_URL": f"http://{ZPODENGINE_HOSTPORT}/api",
