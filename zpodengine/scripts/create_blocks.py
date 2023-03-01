55e6770cc8ade59969fe29e3f0bd993fcf262ee0
Kelby Valenti <kvalenti@vmware.com>
2023-03-01 15:00:51 -0500

    Add component downloader

--- zpodengine/scripts/create_blocks.py
+++ zpodengine/scripts/create_blocks.py
@@ -5,6 +5,9 @@ import os
 from prefect.infrastructure import DockerContainer
 
 ZPODCORE_PATH = os.environ["ZPODCORE_PATH"]
+ZPODFACTORY_LIBRARY_PATH = os.environ["ZPODFACTORY_LIBRARY_PATH"]
+ZPODFACTORY_PRODUCT_PATH = os.environ["ZPODFACTORY_PRODUCT_PATH"]
+ZPODFACTORY_LOG_PATH = os.environ["ZPODFACTORY_LOG_PATH"]
 COMPOSE_PROJECT_NAME = os.environ.get("COMPOSE_PROJECT_NAME", "zpodcore")
 
 
@@ -15,6 +18,9 @@ docker_block = DockerContainer(
         f"{ZPODCORE_PATH}/zpodcommon/src/zpodcommon:/zpodcore/src/zpodcommon",
         f"{ZPODCORE_PATH}/zpodengine/src/zpodengine:/zpodcore/src/zpodengine",
         f"{ZPODCORE_PATH}/.env:/zpodcore/.env",
+        f"{ZPODFACTORY_LIBRARY_PATH}:/library",
+        f"{ZPODFACTORY_PRODUCT_PATH}:/products",
+        f"{ZPODFACTORY_LOG_PATH}:/logs",
     ],
     env={
         "PREFECT_API_URL": "http://zpodengineorion:4200/api",
