8dbd5b4a8a7cf37b7fea9654f791414ab63bcc49
xtuned <dvincent@vmware.com>
2023-03-06 17:26:52 +0000

    integrating component

--- zpodapi/src/zpodapi/components/component_dependencies.py
+++ zpodapi/src/zpodapi/components/component_dependencies.py
@@ -4,16 +4,14 @@ from sqlmodel import Session
 from zpodapi.lib import dependencies
 
 from . import component_services
+from .component_schemas import ComponentUpdate
 
 
 def get_component_record(
     *,
     session: Session = Depends(dependencies.get_session),
-    filename: str | None = None,
+    component_in: ComponentUpdate,
 ):
-    if component := component_services.get(
-        session=session,
-        filename=filename,
-    ):
+    if component := component_services.get(session=session, component_in=component_in):
         return component
     raise HTTPException(status_code=404, detail="Component not found")
--- zpodcommon/src/zpodcommon/models/component_models.py
+++ zpodcommon/src/zpodcommon/models/component_models.py
@@ -5,8 +5,10 @@ class Component(SQLModel, table=True):
     __tablename__ = "components"
 
     id: int | None = Field(default=None, primary_key=True, nullable=False)
+    component_uid: str = Field(..., unique=True, nullable=False)
+    component_name: str = Field(..., unique=False, nullable=False)
+    component_version: str = Field(..., unique=False, nullable=False)
     library_name: str = Field(default=None, foreign_key="libraries.name")
     filename: str = Field(..., unique=True, index=True, nullable=False)
     enabled: bool = Field(False, nullable=False)
-    # status: str = Field(..., nullable=False)
-    # component_uid: Field(..., unique=True, nullable=False)
+    status: str = Field(..., nullable=True)
