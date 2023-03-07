cf2feed61b939c99f075a9c03994560c926de893
xtuned <dvincent@vmware.com>
2023-03-07 21:33:25 +0000

    added detail to error handling

--- zpodapi/src/zpodapi/components/component_utils.py
+++ zpodapi/src/zpodapi/components/component_utils.py
@@ -4,6 +4,6 @@ from pathlib import Path
 
 def get_component(filename: str) -> dict:
     if not Path(filename).exists():
-        raise ValueError("The specified file does not exist")
+        raise ValueError(f"The specified file {filename} does not exist")
     with open(filename, "r") as f:
         return json.load(f)
