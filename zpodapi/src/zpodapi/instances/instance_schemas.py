5fb76dab8367ee72191142188cd87eec772d7a2b
Kelby Valenti <kvalenti@vmware.com>
2023-03-16 13:28:24 -0400

    Had to move instance view to bottom

--- zpodapi/src/zpodapi/instances/instance_schemas.py
+++ zpodapi/src/zpodapi/instances/instance_schemas.py
@@ -12,34 +12,6 @@ from zpodapi.users.user_schemas import UserView
 example_creation_date = datetime(2023, 1, 1)
 
 
-class InstanceView(SQLModel):
-    id: int = Req(example=1)
-    name: str = Req(example="tanzu-lab")
-    description: str = Req(example="Tanzu Lab zPod")
-    domain: str = Req(example="tanzu-lab.maindomain.com")
-    profile: str = Req(example="sddc-profile")
-    creation_date: datetime = Req(example=example_creation_date)
-    last_modified_date: datetime = Req(example=example_creation_date)
-    endpoint: "EndpointView"
-    networks: list["InstanceNetworkView"] = []
-    components: list["InstanceComponentView"] = []
-    permissions: list["InstancePermissionView"] = []
-    features: list["InstanceFeatureView"] = []
-
-
-class InstanceCreate(SQLModel, extra="forbid"):
-    name: str = Req(example="tanzu-lab")
-    description: str = Empty(example="Tanzu Lab zPod")
-    domain: str = Req(example="tanzu-lab.maindomain.com")
-    endpoint_id: int = Req(example=1)
-    profile: str = Req(example="sddc-profile")
-
-
-class InstanceUpdate(SQLModel, extra="forbid"):
-    id: int = Field(None, example=1)
-    description: str = Opt(example="Tanzu Lab zPod")
-
-
 class EndpointView(SQLModel):
     id: int = Req(example=1)
 
@@ -69,3 +41,31 @@ class InstancePermissionView(SQLModel):
     permission: str = Req(example="zpodadmin")
     users: list[UserView] = []
     groups: list[PermissionGroupView] = []
+
+
+class InstanceCreate(SQLModel, extra="forbid"):
+    name: str = Req(example="tanzu-lab")
+    description: str = Empty(example="Tanzu Lab zPod")
+    domain: str = Req(example="tanzu-lab.maindomain.com")
+    endpoint_id: int = Req(example=1)
+    profile: str = Req(example="sddc-profile")
+
+
+class InstanceUpdate(SQLModel, extra="forbid"):
+    id: int = Field(None, example=1)
+    description: str = Opt(example="Tanzu Lab zPod")
+
+
+class InstanceView(SQLModel):
+    id: int = Req(example=1)
+    name: str = Req(example="tanzu-lab")
+    description: str = Req(example="Tanzu Lab zPod")
+    domain: str = Req(example="tanzu-lab.maindomain.com")
+    profile: str = Req(example="sddc-profile")
+    creation_date: datetime = Req(example=example_creation_date)
+    last_modified_date: datetime = Req(example=example_creation_date)
+    endpoint: "EndpointView"
+    networks: list["InstanceNetworkView"] = []
+    components: list["InstanceComponentView"] = []
+    permissions: list["InstancePermissionView"] = []
+    features: list["InstanceFeatureView"] = []
