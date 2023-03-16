887076e9053f39dbfa34443de48077867040c97b
Kelby Valenti <kvalenti@vmware.com>
2023-03-16 13:11:17 -0400

    Reordered

--- zpodapi/src/zpodapi/instances/instance_schemas.py
+++ zpodapi/src/zpodapi/instances/instance_schemas.py
@@ -12,6 +12,21 @@ from zpodapi.users.user_schemas import UserView
 example_creation_date = datetime(2023, 1, 1)
 
 
+class InstanceView(SQLModel):
+    id: int = Req(example=1)
+    name: str = Req(example="tanzu-lab")
+    description: str = Req(example="Tanzu Lab zPod")
+    domain: str = Req(example="tanzu-lab.maindomain.com")
+    profile: str = Req(example="sddc-profile")
+    creation_date: datetime = Req(example=example_creation_date)
+    last_modified_date: datetime = Req(example=example_creation_date)
+    endpoint: "EndpointView"
+    networks: list["InstanceNetworkView"] = []
+    components: list["InstanceComponentView"] = []
+    permissions: list["InstancePermissionView"] = []
+    features: list["InstanceFeatureView"] = []
+
+
 class InstanceCreate(SQLModel, extra="forbid"):
     name: str = Req(example="tanzu-lab")
     description: str = Empty(example="Tanzu Lab zPod")
@@ -29,13 +44,13 @@ class EndpointView(SQLModel):
     id: int = Req(example=1)
 
 
-class InstanceComponentCreate(SQLModel, extra="forbid"):
-    component_uid: str = Req(example="vcda-4.4.1")
-
-
 class InstanceComponentView(SQLModel):
     component: ComponentView
     data: dict[Any, Any]
+
+
+class InstanceComponentCreate(SQLModel, extra="forbid"):
+    component_uid: str = Req(example="vcda-4.4.1")
 
 
 class InstanceFeatureView(SQLModel):
@@ -54,18 +69,3 @@ class InstancePermissionView(SQLModel):
     permission: str = Req(example="zpodadmin")
     users: list[UserView] = []
     groups: list[PermissionGroupView] = []
-
-
-class InstanceView(SQLModel):
-    id: int = Req(example=1)
-    name: str = Req(example="tanzu-lab")
-    description: str = Req(example="Tanzu Lab zPod")
-    domain: str = Req(example="tanzu-lab.maindomain.com")
-    profile: str = Req(example="sddc-profile")
-    creation_date: datetime = Req(example=example_creation_date)
-    last_modified_date: datetime = Req(example=example_creation_date)
-    endpoint: EndpointView
-    networks: list[InstanceNetworkView] = []
-    components: list[InstanceComponentView] = []
-    permissions: list[InstancePermissionView] = []
-    features: list[InstanceFeatureView] = []
