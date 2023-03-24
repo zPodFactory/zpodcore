613919d9c79153b9507e61c339e20af4432ecbf1
Timo Sugliani <timo.sugliani@gmail.com>
2023-03-24 14:56:49 +0100

    Add networks into `Endpoints` and reset alembic + misc fixes
    Signed-off-by: Timo Sugliani <timo.sugliani@gmail.com>

--- zpodcommon/src/zpodcommon/models/library_models.py
+++ zpodcommon/src/zpodcommon/models/library_models.py
@@ -10,4 +10,4 @@ class Library(CommonDatesMixin, SQLModel, table=True):
     name: str = Field(..., unique=True, index=True, nullable=False)
     description: str = Field("", nullable=False)
     git_url: str = Field("", nullable=False)
-    enabled: bool = Field(False, nullable=False)
+    enabled: bool = Field(True, nullable=False)
