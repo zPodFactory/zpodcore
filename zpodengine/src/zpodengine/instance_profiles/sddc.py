d308b182705725cf16999e32181511f9e4626e7f
Timo Sugliani <timo.sugliani@gmail.com>
2023-03-24 11:11:32 +0100

    Update sddc profile to relevant components & order
    Signed-off-by: Timo Sugliani <timo.sugliani@gmail.com>

--- zpodengine/src/zpodengine/instance_profiles/sddc.py
+++ zpodengine/src/zpodengine/instance_profiles/sddc.py
@@ -5,28 +5,26 @@ def instance_profile_flow(
     instance_id,
     wait_for=None,
 ):
+    zbox = add_instance_component(
+        instance_id=instance_id, component_uid="zbox-11.6", wait_for=None
+    )
     esxis = [
         add_instance_component(
             instance_id=instance_id,
-            component_uid="esxi-8.0.0",
+            component_uid="esxi-8.0.0b",
             extra_id=x,
             data=dict(last_octet=x),
-            wait_for=wait_for,
+            wait_for=zbox,
         )
         for x in range(11, 14)
     ]
     vcsa = add_instance_component(
         instance_id=instance_id,
-        component_uid="vcsa-8.0.0",
+        component_uid="vcsa-8.0.0b",
         wait_for=[esxis],
-    )
-    nsxt = add_instance_component(
-        instance_id=instance_id,
-        component_uid="nsxt-4.3.1",
-        wait_for=[vcsa],
     )
     add_instance_component(
         instance_id=instance_id,
-        component_uid="hcx-2.6.3",
-        wait_for=[nsxt],
+        component_uid="nsx-4.1.0.0",
+        wait_for=[vcsa],
     )
