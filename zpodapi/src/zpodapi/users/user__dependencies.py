59da4be6d461abc4774cc1f398c04308fddfcda9
Kelby Valenti <kvalenti@vmware.com>
2023-03-28 16:11:14 -0400

    More Annotated updates

--- zpodapi/src/zpodapi/users/user__dependencies.py
+++ zpodapi/src/zpodapi/users/user__dependencies.py
@@ -12,14 +12,17 @@ from .user__types import UserIdType
 def get_user_record(
     *,
     session: dependencies.GetSession,
-    id: UserIdType = Path(
-        examples={
-            "id": {"value": "1"},
-            "id alternative": {"value": "id=1"},
-            "username": {"value": "username=jdoe"},
-            "email": {"value": "email=jdoe@example.com"},
-        },
-    ),
+    id: Annotated[
+        UserIdType,
+        Path(
+            examples={
+                "id": {"value": "1"},
+                "id alternative": {"value": "id=1"},
+                "username": {"value": "username=jdoe"},
+                "email": {"value": "email=jdoe@example.com"},
+            },
+        ),
+    ],
 ):
     if user := UserService(session=session).get(value=id):
         return user
