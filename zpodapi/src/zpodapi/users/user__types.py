3d84055ec952d420d9731fa3ffeb291827810f00
Kelby Valenti <kvalenti@vmware.com>
2023-03-28 10:44:55 -0400

    Refactored IDType validation/parsing

--- zpodapi/src/zpodapi/users/user__types.py
+++ zpodapi/src/zpodapi/users/user__types.py
@@ -1,24 +1,7 @@
 from pydantic import EmailStr
 
+from zpodapi.lib.id_type_base import IdType
 
-class UserIdType(str):
-    @classmethod
-    def __get_validators__(cls):
-        yield cls.validate
 
-    @classmethod
-    def validate(cls, v):
-        if not isinstance(v, str):
-            raise TypeError("string required")
-        v = v.lower()
-        idtype, id_ = (v if "=" in v else f"id={v}").split("=")
-        if idtype not in ("id", "username", "email"):
-            raise ValueError(f"Invalid ID Type: {idtype}")
-        if idtype == "id":
-            int(id_)
-        elif idtype == "email":
-            EmailStr.validate(id_)
-        return cls(v)
-
-    def __repr__(self):
-        return f"UserId({super().__repr__()})"
+class UserIdType(IdType):
+    fields = dict(id=int, username=str, email=EmailStr)
