5280da4b95ce17d4a2c7231690063cc2e766c824
xtuned <dvincent@vmware.com>
2023-03-29 18:51:07 +0000

    added library helper functions

--- zpodcli/src/zpodcli/cmd/library.py
+++ zpodcli/src/zpodcli/cmd/library.py
@@ -8,40 +8,100 @@ from zpodcli.lib.callback import isauthenticated
 
 app = typer.Typer(help="Manage libraries", callback=isauthenticated)
 
+console = Console()
+
+ZPOD_LIBRARY_DESCRIPTION = "Default zPodFactory library"
+ZPOD_LIBRARY_GIT_URL = "https://github.com/zpodfactory/zpodlibrary"
+
+
+def generate_table(libraries: list, action: str = None):
+    title = f"{action} Library"
+
+    table = Table(
+        title=title,
+        show_header=True,
+        header_style="bold cyan",
+        title_style="bold green",
+    )
+    table.add_column("Name")
+    table.add_column("Decription", style="dim")
+    table.add_column("Git URL")
+    table.add_column("Creation Date", style="dim")
+    table.add_column("Last Update", style="dim")
+    table.add_column("Enabled")
+    for library in libraries:
+        table.add_row(
+            f"[green]{library.name}[/green]",
+            f"[magenta]{library.description}[/magenta]",
+            library.git_url,
+            f"[yellow]{library.creation_date.strftime('%Y-%m-%d %H:%M:%S')}[/yellow]",
+            f"[magenta]{library.last_modified_date.strftime('%Y-%m-%d %H:%M:%S')}[/magenta]",
+            library.enabled.__str__(),
+        )
+    console.print(table)
+
 
 @app.command()
 def list():
-    """
-    List libraries
-    """
-    print("Listing libraries")
-
     z = zpod_client.ZpodClient()
     libraries = z.libraries_get_all.sync()
+    generate_table(libraries=libraries, action="List")
 
-    print("Server Library list")
-    print(libraries)
 
-    print("")
-    print("[yellow][blink]Rich Table nice view ;-)[/blink][/yellow]")
+@app.command()
+def create(
+    name: str = typer.Option(..., "--name", "-n"),
+    git_url: str = typer.Option(ZPOD_LIBRARY_GIT_URL, "--git_url", "-u"),
+    description: str = typer.Option(ZPOD_LIBRARY_DESCRIPTION, "--description", "-d"),
+):
+    z = zpod_client.ZpodClient()
+    library_in = LibraryCreate(name=name, description=description, git_url=git_url)
+    library = z.libraries_create.sync(json_body=library_in)
+    generate_table(libraries=[library], action="Create")
 
-    table = Table(title="Library List", show_header=True, header_style="bold cyan")
-    table.add_column("Name")
-    table.add_column("Description")
-    table.add_column("Git URL")
-    table.add_column("Creation Date")
-    table.add_column("Last Update")
-    table.add_column("Enabled")
 
-    for library in libraries:
-        table.add_row(
-            library.name,
-            library.description,
-            library.git_url,
-            library.creation_date.strftime("%Y-%m-%d %H:%M:%S"),
-            library.last_modified_date.strftime("%Y-%m-%d %H:%M:%S"),
-            library.enabled.__str__(),
+@app.command()
+def delete(
+    name: str = typer.Option(..., "--name", "-n"),
+    git_url: str = typer.Option(ZPOD_LIBRARY_GIT_URL, "--git_url", "-u"),
+):
+    z = zpod_client.ZpodClient()
+    library = z.libraries_delete.sync(name=name, git_url=git_url)
+    if library is None:
+        console.print(
+            f"Library [magenta]{name}[/magenta] has been deleted successfully",
+            style="green",
         )
+    else:
+        console.print(f"Error {library}", style="red")
 
-    console = Console()
-    console.print(table)
+
+@app.command()
+def update(
+    enabled: Optional[bool] = typer.Option(None, "--enable/--disable"),
+    name: str = typer.Option(..., "--name", "-n"),
+    description: str = typer.Option(ZPOD_LIBRARY_DESCRIPTION, "--description", "-d"),
+):
+    is_enabled = None
+    z = zpod_client.ZpodClient()
+    if enabled is None:
+        libraries = z.libraries_get_all.sync()
+        library = [library for library in libraries if library.name == name][0]
+        is_enabled = library.enabled
+    elif enabled:
+        is_enabled = True
+    else:
+        is_enabled = False
+
+    library_in = LibraryUpdate(enabled=is_enabled, description=description)
+    if library := z.libraries_update.sync(json_body=library_in, name=name):
+        generate_table(libraries=[library], action="Enable")
+    else:
+        console.print(f"Error {library}", style="red")
+
+
+@app.command()
+def get(name: str = typer.Option(..., "--name", "-n")):
+    z = zpod_client.ZpodClient()
+    library = z.libraries_get.sync(name=name)
+    generate_table(libraries=[library], action="Get")
