12f2093778454f50c121339131c830a2deb80268
Kelby Valenti <kvalenti@vmware.com>
2023-04-12 10:32:54 -0400

    Use enum for instance permission

--- zpodcommon/src/zpodcommon/enums.py
+++ zpodcommon/src/zpodcommon/enums.py
@@ -1,12 +1,6 @@
 from enum import Enum
 
 
-class InstanceStatus(str, Enum):
-    ACTIVE = "ACTIVE"
-    DELETED = "DELETED"
-    PENDING = "PENDING"
-
-
 class ComponentStatus(str, Enum):
     FAILED_DOWNLOAD = "FAILED_DOWNLOAD"
     NOT_ENTITLED = "NOT_ENTITLED"
@@ -14,3 +8,15 @@ class ComponentStatus(str, Enum):
     FAILED_AUTHENTICATION = "FAILED_AUTHENTICATION"
     SCHEDULED = "SCHEDULED"
     DOWNLOAD_INCOMPLETE = "DOWNLOAD_INCOMPLETE"
+
+
+class InstancePermission(str, Enum):
+    INSTANCE_ADMIN = "INSTANCE_ADMIN"
+    INSTANCE_OWNER = "INSTANCE_OWNER"
+    INSTANCE_READ_ONLY = "INSTANCE_READ_ONLY"
+
+
+class InstanceStatus(str, Enum):
+    ACTIVE = "ACTIVE"
+    DELETED = "DELETED"
+    PENDING = "PENDING"
