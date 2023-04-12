ef5cfcd42915cfb20e53fee4bad27d0567a61b57
Timo Sugliani <timo.sugliani@gmail.com>
2023-04-12 18:07:06 +0200

    WIP stuff around zpodengine for initial instance deployment
    Signed-off-by: Timo Sugliani <timo.sugliani@gmail.com>

--- zpodengine/src/zpodengine/instance/instance_dnsmasq.py
+++ zpodengine/src/zpodengine/instance/instance_dnsmasq.py
@@ -1,6 +1,9 @@
+from ipaddress import IPv4Network
+
 from prefect import task
 
 from zpodcommon import models as M
+from zpodcommon.lib.network import create_dnsmasq_config, get_instance_ip_info
 from zpodengine.lib import database
 
 
@@ -11,4 +14,12 @@ def instance_dnsmasq(
 ):
     with database.get_session_ctx() as session:
         instance = session.get(M.Instance, instance_id)
-        print(f"Set DNSMASQ using the {instance.networks[0].cidr} network")
+
+        # Fetch Instance mgmt subnet (first subnet)
+        subnet_mgmt = IPv4Network(instance.networks[0].cidr)
+
+        # Fetch associate zbox IP from subnet
+        dns_ip = get_instance_ip_info(subnet_mgmt, "zbox")
+
+        # Create dnsmasq configuration
+        create_dnsmasq_config(instance.name, instance.domain, dns_ip)
