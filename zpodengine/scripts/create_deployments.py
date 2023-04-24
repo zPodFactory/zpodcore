4fea8d9f0e1b44aa44d2adddc5fb473b0075bc5d
Kelby Valenti <kvalenti@vmware.com>
2023-04-24 16:34:12 -0400

    Simplify deployment definition

--- zpodengine/scripts/create_deployments.py
+++ zpodengine/scripts/create_deployments.py
@@ -2,14 +2,6 @@
 # flake8: noqa
 from zpodengine.lib.deployments import create_deployment
 
-create_deployment(
-    flow="component_download.flow_component_download.flow_component_download",
-)
-
-create_deployment(
-    flow="instance_deploy.flow_instance_deploy.flow_instance_deploy",
-)
-
-create_deployment(
-    flow="instance_component_add.flow_instance_component_add.flow_instance_component_add",
-)
+create_deployment(flow="component_download")
+create_deployment(flow="instance_deploy")
+create_deployment(flow="instance_component_add")
--- zpodengine/src/zpodengine/lib/deployments.py
+++ zpodengine/src/zpodengine/lib/deployments.py
@@ -1,15 +1,36 @@
 import importlib
+import inspect
 
+from prefect import Flow
 from prefect.deployments import Deployment
 from prefect.infrastructure import DockerContainer
 
 
-def create_deployment(*, flow: str, **kwargs):
-    def get_flow(flow):
-        module_name, method = flow.rsplit(".", 1)
-        mod = importlib.import_module(f"zpodengine.{module_name}")
-        flow_method = getattr(mod, method)
-        flow_filepath = getattr(mod, "__file__", None)
+def create_deployment(*, flow: str | Flow, **kwargs):
+    """
+        * flow=[module].[method_name]
+          ex.
+            flow="instance_deploy.flow_instance_deploy.flow_instance_deploy"
+        * flow=[deployment_name]
+          ex.
+            flow="instance_deploy".  This is equal to "instance_deploy.flow_instance_deploy.flow_instance_deploy".
+        * flow=method
+          ex.
+            from zpodengine.instance_deploy.flow_instance_deploy import flow_instance_deploy
+            flow=flow_instance_deploy
+    """  # noqa: E501, B950
+
+    def get_flow(flow_):
+        if type(flow_) == str:
+            if "." not in flow_:
+                flow_ = f"{flow_}.flow_{flow_}.flow_{flow_}"
+            module_name, method = flow_.rsplit(".", 1)
+            mod = importlib.import_module(f"zpodengine.{module_name}")
+            flow_method = getattr(mod, method)
+            flow_filepath = getattr(mod, "__file__", None)
+        else:
+            flow_method = flow_
+            flow_filepath = inspect.getfile(flow_.fn)
         return dict(
             flow=flow_method,
             entrypoint=f"{flow_filepath}:{flow_method.fn.__name__}",
@@ -25,7 +46,7 @@ def create_deployment(*, flow: str, **kwargs):
             version=1,
             work_queue_name="default",
         )
-        | (get_flow(flow) if type(flow) == str else dict(flow=flow))
+        | get_flow(flow)
         | kwargs
     )
     result = Deployment.build_from_flow(**data)
