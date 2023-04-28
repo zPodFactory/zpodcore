b0d6b3902b716b1e6e4f8f1383b95ddf1cbec171
Timo Sugliani <timo.sugliani@gmail.com>
2023-04-28 19:24:51 +0200

    Fixing components to avoid boiler plate code for filename vs jsonfile
    Signed-off-by: Timo Sugliani <timo.sugliani@gmail.com>

--- zpodapi/src/zpodapi/components/component__utils.py
+++ zpodapi/src/zpodapi/components/component__utils.py
@@ -2,8 +2,8 @@ import json
 from pathlib import Path
 
 
-def get_component(filename: str) -> dict:
-    if not Path(filename).exists():
-        raise ValueError(f"The specified file {filename} does not exist")
-    with open(filename, "r") as f:
+def get_component(jsonfile: str) -> dict:
+    if not Path(jsonfile).exists():
+        raise ValueError(f"The specified file {jsonfile} does not exist")
+    with open(jsonfile, "r") as f:
         return json.load(f)
