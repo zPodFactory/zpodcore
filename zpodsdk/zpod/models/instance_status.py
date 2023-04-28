3e55c597b807deee3151862f89893488f03390d8
Kelby Valenti <kvalenti@vmware.com>
2023-04-28 13:52:43 -0400

    Instance schema updates

--- zpodsdk/zpod/models/instance_status.py
+++ zpodsdk/zpod/models/instance_status.py
@@ -3,9 +3,11 @@ from enum import Enum
 
 class InstanceStatus(str, Enum):
     ACTIVE = "ACTIVE"
+    BUILDING = "BUILDING"
     DELETING = "DELETING"
     DELETED = "DELETED"
     PENDING = "PENDING"
+    FAILED = "FAILED"
 
     def __str__(self) -> str:
         return str(self.value)
