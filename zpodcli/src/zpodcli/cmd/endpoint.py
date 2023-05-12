9bde4dff499b591197396f4cfadfafdee5da8a37
Timo Sugliani <timo.sugliani@gmail.com>
2023-05-12 12:17:32 +0200

    zpodcli updates and small consistency alignment
    Signed-off-by: Timo Sugliani <timo.sugliani@gmail.com>

--- zpodcli/src/zpodcli/cmd/endpoint.py
+++ zpodcli/src/zpodcli/cmd/endpoint.py
@@ -1,6 +1,49 @@
 import typer
+from rich import print
+from rich.console import Console
+from rich.pretty import Pretty
+from rich.table import Table
+from zpod.models.endpoint_compute_view import EndpointComputeView
+from zpod.models.endpoint_network_view import EndpointNetworkView
+from zpod.models.endpoint_view_full import EndpointViewFull
+
+from zpodcli.lib import zpod_client
 
 app = typer.Typer(help="Manage endpoints")
+
+
+console = Console()
+
+
+def generate_table(endpoints: list, action: str = None):
+    title = f"{action} Endpoints"
+
+    table = Table(
+        title=title,
+        title_style="bold",
+        show_header=True,
+        header_style="bold cyan",
+    )
+    table.add_column("Id")
+    table.add_column("Name")
+    table.add_column("Description")
+    table.add_column("Compute")
+    table.add_column("Network")
+
+    for endpoint in endpoints:
+        ep = endpoint.endpoints
+        epc = ep.compute
+        epn = ep.network
+
+        table.add_row(
+            endpoint.id,
+            endpoint.name,
+            endpoint.description,
+            Pretty(epc),
+            Pretty(epn),
+            f"{endpoint.enabled.__str__()}",
+        )
+    console.print(table)
 
 
 @app.command()
@@ -8,4 +51,6 @@ def list():
     """
     List Endpoints
     """
-    print("Listing endpoints")
+    z = zpod_client.ZpodClient()
+    endpoints = z.endpoints_get_all.sync()
+    generate_table(endpoints, "List")
