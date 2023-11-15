be284a530814d69b7c219187665261226a704628
Timo Sugliani <timo.sugliani@gmail.com>
2023-11-15 18:03:06 +0000

    Disable concurrency for NSX Projects API calls
    Signed-off-by: Timo Sugliani <timo.sugliani@gmail.com>

--- zpodengine/src/zpodengine/instance_destroy/instance_destroy_4_networking.py
+++ zpodengine/src/zpodengine/instance_destroy/instance_destroy_4_networking.py
@@ -9,12 +9,16 @@ SEGMENT_MAX_WAIT_FOR_EMPTY = 120
 SEGMENT_WAIT_BETWEEN_TRIES = 5
 
 
-@task
+@task(tags=["atomic_operation"])
 def instance_destroy_networking(instance_id: int):
     print("Destroy top level networking")
 
     with database.get_session_ctx() as session:
         instance = session.get(M.Instance, instance_id)
+
+        # Destroy Project
+        # This operation does not support concurrent calls.
+        # Adding tags["atomic_operation"] to task will disable concurrency
         with NsxProjectDestroy(
             project_id=f"{settings.SITE_ID}-{instance.name}-project",
             endpoint=instance.endpoint,
