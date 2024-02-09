2b4ce802afeccd5163e9d12c3a79663698638f5c
Kelby Valenti <kvalenti@vmware.com>
2024-02-09 19:17:53 +0000

    Non-project based nsxt

--- zpodengine/src/zpodengine/instance_destroy/instance_destroy_4_networking.py
+++ zpodengine/src/zpodengine/instance_destroy/instance_destroy_4_networking.py
@@ -1,30 +1,25 @@
 from prefect import task
 
 from zpodcommon import models as M
-from zpodengine import settings
 from zpodengine.lib import database
-from zpodengine.nsx_project_destroy.nsx_project_destroy import NsxProjectDestroy
 
-SEGMENT_MAX_WAIT_FOR_EMPTY = 120
-SEGMENT_WAIT_BETWEEN_TRIES = 5
+from .networking_destroy_nsxt import networking_destroy_nsxt
+from .networking_destroy_nsxt_project import networking_destroy_nsxt_project
 
 
 @task(tags=["atomic_operation"])
+# This operation does not support concurrent calls.
+# Adding tags["atomic_operation"] to task will disable concurrency
 def instance_destroy_networking(instance_id: int):
     print("Destroy top level networking")
 
     with database.get_session_ctx() as session:
-        instance = session.get(M.Instance, instance_id)
-
-        # Destroy Project
-        # This operation does not support concurrent calls.
-        # Adding tags["atomic_operation"] to task will disable concurrency
-        with NsxProjectDestroy(
-            project_id=f"{settings.SITE_ID}-{instance.name}-project",
-            endpoint=instance.endpoint,
-        ) as npd:
-            npd()
-
-
-if __name__ == "__main__":
-    instance_destroy_networking.fn(5)
+        instance: M.Instance = session.get(M.Instance, instance_id)
+        driver = instance.endpoint.endpoints["network"]["driver"]
+        match driver:
+            case "nsxt":
+                return networking_destroy_nsxt(instance)
+            case "nsxt-projects":
+                return networking_destroy_nsxt_project(instance)
+            case _:
+                raise f"Unknown driver: {driver}"
