3929e278bc4c2f98ec87425e1d6c76c24cbc7cb4
Kelby Valenti <kvalenti@vmware.com>
2024-03-07 17:33:07 +0000

    Version v0.3.0

--- zpodsdk/zpod/__init__.py
+++ zpodsdk/zpod/__init__.py
@@ -1,7 +1,7 @@
 """ A client library for accessing zPod API """
 from .client import AuthenticatedClient, Client
 
-__version__ = "0.2.12"
+__version__ = "0.3.0"
 
 __all__ = (
     "AuthenticatedClient",
