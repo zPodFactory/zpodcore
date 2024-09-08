1b31cae401088451e5617ade039fde330fd49c04
Timo Sugliani <timo.sugliani@gmail.com>
2024-09-08 11:22:40 +0000

    Add Feature Flag `ff_esxi_hostname_is_fqdn`
    Signed-off-by: Timo Sugliani <timo.sugliani@gmail.com>

--- zpodengine/src/zpodengine/lib/ovfdeployer.py
+++ zpodengine/src/zpodengine/lib/ovfdeployer.py
@@ -76,9 +76,19 @@ def ovf_deployer(zpod_component: M.ZpodComponent):
     url = f"https://{username}:{password}@{hostname}/sdk"
     print(f"Deploying to [https://{username}:XXXXXXXX@{hostname}/sdk]...")
 
+    # FF (Feature Flag) support for esxi hostname = fqdn (William Lam templates)
+    zpod_component_hostname = zpod_component.hostname
+    if component.component_json[
+        "component_type"
+    ] == "esxi" and DBUtils.get_setting_value("ff_esxi_hostname_is_fqdn"):
+        print(
+            f"[ff_esxi_hostname_is_fqdn] Setting hostname to: {zpod_component.fqdn}..."
+        )
+        zpod_component_hostname = zpod_component.fqdn
+
     t = Template(json.dumps(govc_spec))
     govc_spec_render = t.render(
-        zpod_hostname=zpod_component.hostname,
+        zpod_hostname=zpod_component_hostname,
         zpod_ipaddress=zpod_component.ip,
         zpod_netmask=zpod_netmask,
         zpod_netprefix=ZPOD_PUBLIC_SUB_NETWORKS_PREFIXLEN,
